df6630e2ebfe777579ee10bcb9069bf6efec9767
Ranjan Kumar <111798933+ranjankumar555@users.noreply.github.com>
2025-01-21 04:30:59 +0530

    Update timer.c

--- delay-generation/timer.c
+++ delay-generation/timer.c
@@ -6,18 +6,18 @@ extern void delay_sec(const unsigned int sec);
 extern void delay_ms(const unsigned int x){
 	T0PC = 0;        		// set the starting count value
 	T0PR = 15000-1;			// set the ending count value
-	T0TCR = 1;					// enable the timer
-	T0TC = 0;						// set timer counter to zero
+	T0TCR = 1;			// enable the timer
+	T0TC = 0;			// set timer counter to zero
 	while(T0TC < x); 		// busy the processor for the x millisecond
-	T0TCR = 0;					// disable the timer
+	T0TCR = 0;			// disable the timer
 }
 
 /* function to generate delay for exact x second */
 extern void delay_sec(const unsigned int x){
-	T0PC = 0;								// set the starting count value
+	T0PC = 0;				// set the starting count value
 	T0PR = 15000000-1;			// set the ending count value
-	T0TCR = 1;							// enable the timer
-	T0TC = 0;								// set timer counter to zero									
-	while(T0TC < x);				// busy the processor for the x second
-	T0TCR = 0;							// disable the timer
+	T0TCR = 1;				// enable the timer
+	T0TC = 0;				// set timer counter to zero									
+	while(T0TC < x);			// busy the processor for the x second
+	T0TCR = 0;				// disable the timer
 }
